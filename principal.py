e9272e247a73c9e6ac380dad4eaf594ea4abdb42
carinealencar <carinealencar@outlook.com>
2025-12-10 14:17:25 -0300

    principal.py

---
+++ principal.py
@@ -0,0 +1,9 @@
+import os
+
+# Configuração da página
+st.set_page_config(
+    page_title="Dashboard das notas do Enem nos últimos 5 anos",
+    page_icon="📊",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
